b1aae14238ef73c927556ed1b5f80dfa6500e09b
csjianga <csjianga@isoftstone.com>
2019-05-05 09:37:23 +0800

    fix

--- src/api/verification.js
+++ src/api/verification.js
@@ -33,12 +33,6 @@ export function isHave(str) {
   const reg = /^\S+$/
   return reg.test(str)
 }
-
-// 空格（曦哥
-export function isHaveBlank(str) {
-  const reg = /\s+/
-  return reg.test(str)
-}
 // 不能输入表情
 export function istextarea(str) {
   const reg = /[^\u0020-\u007E\u00A0-\u00BE\u2E80-\uA4CF\uF900-\uFAFF\uFE30-\uFE4F\uFF00-\uFFEF\u0080-\u009F\u2000-\u201f\u2026\u2022\u20ac\r\n]/g
